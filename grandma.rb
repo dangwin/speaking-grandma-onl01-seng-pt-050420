049763a0dfbc27bf3526e4eb1d07fec5bf09f700
Dang Nguyen <danghwin@gmail.com>
2020-05-07 01:56:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,10 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase ==
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
