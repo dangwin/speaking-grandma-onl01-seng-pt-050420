580bf8b96b0153fb445781d318226b34b47a47e5
Dang Nguyen <danghwin@gmail.com>
2020-05-07 02:13:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-    elsif phrase == "I LOVE YOU GRANDMA!"
+  
+  
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  if phrase == phrase.upcase 
+  elsif phrase == phrase.upcase 
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
